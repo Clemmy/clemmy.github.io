ae74baa332544b2e7f9a6b6e9ee3fd82ab6a182e
Clement Hoang <clement.hoang24@gmail.com>
2022-06-04 07:40:24 +0800

    Use dynamic height instead of weird flexbox solution

--- src/components/FlippableAvatar.tsx
+++ src/components/FlippableAvatar.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { css } from '@emotion/react';
+import { useWindowSize } from '../hooks/useWindowSize';
 
+// Flipping based on https://davidwalsh.name/css-flip
 const containerStyles = css`
   perspective: 2000px;
 
@@ -13,16 +15,14 @@ const containerStyles = css`
     transition: 0.5s;
     transform-style: preserve-3d;
 
-    display: flex;
-    width: 100%;
-    align-items: stretch;
-    will-change: transform;
+    position: relative;
   }
 
   .front {
     backface-visibility: hidden;
-    width: 100%;
-    flex: 0 0 auto;
+    position: absolute;
+    top: 0;
+    left: 0;
 
     z-index: 2;
     transform: rotateY(0deg);
@@ -30,18 +30,30 @@ const containerStyles = css`
 
   .back {
     backface-visibility: hidden;
-    width: 100%;
-    flex: 0 0 auto;
-    margin-left: -100%;
+    position: absolute;
+    top: 0;
+    left: 0;
 
     transform: rotateY(180deg);
   }
 `;
 
 export function FlippableAvatar() {
+  const size = useWindowSize();
+  const [containerHeight, setContainerHeight] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      setContainerHeight(containerRef.current.clientWidth);
+    }
+  }, [size]);
+
   return (
     <div
+      ref={containerRef}
       css={containerStyles}
+      style={{ height: containerHeight }}
       className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 rounded-full"
     >
       <div className="flipper">
